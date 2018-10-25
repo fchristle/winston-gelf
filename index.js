c5ca8b29db053fb4101d7fc2078f45118b421627
Fatima Gomez <fatima.gomez@icharts.net>
2018-10-25 00:29:56 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const Transport = require('winston-transport');
+const logger = require('gelf-pro');
+
+const levels = {
+  emerg: 'emergency',
+  alert: 'alert',
+  crit: 'critical',
+  error: 'error',
+  warning: 'warning',
+  notice: 'notice',
+  info: 'info',
+  debug: 'debug'
+}
+
+class GelfTransport extends Transport {
+  constructor(opts) {
+    super(opts);
+    this.logger = logger;
+    this.logger.setConfig(opts.gelfPro);
+  }
+
+  log(info, callback) {
+    setImmediate(() => {
+      this.emit('logged', info);
+    });
+
+    const {level, message} = info;
+    const graylogLevel = levels[level] || levels.info;
+    this.logger[graylogLevel](message);
+
+    callback();
+  }
+}
+
+module.exports = exports = GelfTransport
